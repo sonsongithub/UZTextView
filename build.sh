4b95eb37417ac371700be622343572157272b6ee
sonson <yyoshida@d-itlab.co.jp>
2013-08-20 09:24:11 +0900

    Added the build script for library files. Removed old document files.

---
+++ build.sh
@@ -0,0 +1,14 @@
+xcodebuild -sdk iphoneos -arch armv7 -arch armv7s clean build
+xcodebuild -sdk iphonesimulator -arch i386 clean build
+
+mkdir -p ./Product
+xcrun lipo -create build/Release-iphonesimulator/libUZTextView.a build/Release-iphoneos/libUZTextView.a -output Product/libUZTextView.a
+cp ./UZTextView/UZTextView.h ./Product/
+
+if [ -x /usr/local/bin/appledoc ]; then
+	echo "appledoc is found and executable, will start to create documentation."
+	appledoc --project-name UZTextView --project-company sonson --create-html --no-create-docset --output ./Product/ ./UZTextView/
+	mv ./Product/html ./Product/document
+else
+	echo "appledoc is not found"
+fi
