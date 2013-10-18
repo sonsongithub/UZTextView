146e82754710ff05efb8a88238033f400a28166f
Yuichi Yoshida <yoshida.yuichi@gmail.com>
2013-10-19 06:14:12 +0900

    Adjust the size of cursor to select text.

--- UZTextView/UZCursorView.m
+++ UZTextView/UZCursorView.m
@@ -8,25 +8,27 @@
 
 #import "UZCursorView.h"
 
-#define UZ_CURSOR_BALL_RADIUS	6
-#define UZ_CURSOR_POLE_WIDTH	2
+#define UZ_CURSOR_BALL_RADIUS			4
+#define UZ_CURSOR_POLE_WIDTH			2
+#define UZ_CURSOR_POLE_Y_MARGIN			7
 
-#define UZ_CURSOR_HORIZONTAL_MARGIN 10
-#define UZ_CURSOR_VERTICAL_MARGIN 10
+#define UZ_CURSOR_HORIZONTAL_MARGIN1	30
+#define UZ_CURSOR_HORIZONTAL_MARGIN2	10
+#define UZ_CURSOR_VERTICAL_MARGIN		20
 
 @implementation UZCursorView
 
 + (CGRect)cursorRectWithEdgeRect:(CGRect)rect cursorDirection:(UZTextViewCursorDirection)direction {
 	if (direction == UZTextViewUpCursor) {
-		return CGRectMake(rect.origin.x - UZ_CURSOR_BALL_RADIUS - UZ_CURSOR_HORIZONTAL_MARGIN,
+		return CGRectMake(rect.origin.x - UZ_CURSOR_HORIZONTAL_MARGIN1,
 						  rect.origin.y - UZ_CURSOR_VERTICAL_MARGIN,
-						  UZ_CURSOR_HORIZONTAL_MARGIN + UZ_CURSOR_BALL_RADIUS * 2,
+						  UZ_CURSOR_HORIZONTAL_MARGIN1 + UZ_CURSOR_HORIZONTAL_MARGIN2,
 						  rect.size.height + UZ_CURSOR_VERTICAL_MARGIN * 2);
 	}
 	else {
-		return CGRectMake(rect.origin.x - UZ_CURSOR_BALL_RADIUS,
+		return CGRectMake(rect.origin.x - UZ_CURSOR_HORIZONTAL_MARGIN2,
 						  rect.origin.y - UZ_CURSOR_VERTICAL_MARGIN,
-						  UZ_CURSOR_HORIZONTAL_MARGIN + UZ_CURSOR_BALL_RADIUS * 2,
+						  UZ_CURSOR_HORIZONTAL_MARGIN1 + UZ_CURSOR_HORIZONTAL_MARGIN2,
 						  rect.size.height + UZ_CURSOR_VERTICAL_MARGIN * 2);
 	}
 }
@@ -35,12 +37,6 @@
 	self = [super initWithFrame:CGRectZero];
 	_direction = direction;
 	self.backgroundColor = [UIColor clearColor];
-	
-#ifdef _UZTEXTVIEW_DEBUG_
-	// for debug
-	self.backgroundColor = [[UIColor greenColor] colorWithAlphaComponent:0.5];
-#endif
-	
 	return self;
 }
 
@@ -54,19 +50,25 @@
 	
 	CGRect lineRect;
 	CGPoint circleCenter;
+
+#ifdef _UZTEXTVIEW_DEBUG_
+	// for debug
+	[[UIColor blueColor] setStroke];
+	CGContextStrokeRect(context, CGRectInset(rect, 1, 1));
+#endif
 	
 	if (_direction == UZTextViewUpCursor) {
-		circleCenter = CGPointMake(rect.size.width - UZ_CURSOR_BALL_RADIUS, rect.origin.y + UZ_CURSOR_BALL_RADIUS);
+		circleCenter = CGPointMake(UZ_CURSOR_HORIZONTAL_MARGIN1 - 1, UZ_CURSOR_VERTICAL_MARGIN - UZ_CURSOR_POLE_Y_MARGIN);
 		lineRect = CGRectMake(
 							  circleCenter.x - UZ_CURSOR_POLE_WIDTH/2, circleCenter.y,
-							  UZ_CURSOR_POLE_WIDTH, rect.size.height - circleCenter.y
+							  UZ_CURSOR_POLE_WIDTH, rect.size.height - UZ_CURSOR_VERTICAL_MARGIN*2 + UZ_CURSOR_POLE_Y_MARGIN
 							  );
 	}
 	else {
-		circleCenter = CGPointMake(UZ_CURSOR_BALL_RADIUS, rect.origin.y + rect.size.height - UZ_CURSOR_BALL_RADIUS);
+		circleCenter = CGPointMake(UZ_CURSOR_HORIZONTAL_MARGIN2, rect.size.height - (UZ_CURSOR_VERTICAL_MARGIN - UZ_CURSOR_POLE_Y_MARGIN));
 		lineRect = CGRectMake(
-							  circleCenter.x - UZ_CURSOR_POLE_WIDTH/2, rect.origin.y,
-							  UZ_CURSOR_POLE_WIDTH, rect.origin.y + rect.size.height
+							  circleCenter.x - UZ_CURSOR_POLE_WIDTH/2, circleCenter.y - (rect.size.height - UZ_CURSOR_VERTICAL_MARGIN*2 + UZ_CURSOR_POLE_Y_MARGIN),
+							  UZ_CURSOR_POLE_WIDTH, rect.size.height - UZ_CURSOR_VERTICAL_MARGIN*2 + UZ_CURSOR_POLE_Y_MARGIN
 							  );
 	}
 	CGContextAddArc(context, circleCenter.x, circleCenter.y, UZ_CURSOR_BALL_RADIUS, 0, 2 * M_PI, 0);
