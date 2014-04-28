59e47c119e90aeabb94aefe6d890ec8d71cf58fa
Yuichi Yoshida <yoshida.yuichi@gmail.com>
2014-04-29 05:26:06 +0900

    Updated README and build script.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-xcodebuild -sdk iphoneos -arch armv7 -arch armv7s clean build
-xcodebuild -sdk iphonesimulator -arch i386 clean build
+xcodebuild -sdk iphoneos -arch armv7 -arch armv7s -arch arm64 clean build
+xcodebuild -sdk iphonesimulator -arch i386 -arch x86_64 clean build
 
 xcrun lipo -create build/Release-iphonesimulator/libUZTextView.a build/Release-iphoneos/libUZTextView.a -output build/libUZTextView.a
 cp ./UZTextView/UZTextView.h ./build/
